ff3a1e61d59b3ca48b7c6f16201e21ae78bbee40
IIIAKO <iiiako197@gmail.com>
2023-11-14 17:05:45 +0200

    Home work 1

---
+++ 1/HomeWork1/ConsoleСalculatorUser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace ConsoleСalculatorUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter expression in the following format: \"<int operand1> <operator> <int operand2>\" : ");
+            string input = Console.ReadLine();
+
+            string[] expression = input.Split(' ');
+
+            int result;
+
+            Process simpleConsoleCalculator = new Process();
+
+            simpleConsoleCalculator.StartInfo.FileName = "SimpleConsoleCalculator.exe";
+            simpleConsoleCalculator.StartInfo.Arguments = input;
+
+            simpleConsoleCalculator.StartInfo.RedirectStandardOutput = true;
+            simpleConsoleCalculator.StartInfo.CreateNoWindow = true;
+
+            simpleConsoleCalculator.Start();
+            simpleConsoleCalculator.WaitForExit();
+
+            result = simpleConsoleCalculator.ExitCode;
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
