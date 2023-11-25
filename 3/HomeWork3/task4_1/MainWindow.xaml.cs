82ea9614b55f3df9b596f5bf9223cc6b668a5c27
IIIAKO <iiiako197@gmail.com>
2023-11-25 16:46:06 +0200

    hw4 fix

--- 3/HomeWork3/task4_1/MainWindow.xaml.cs
+++ 3/HomeWork3/task4_1/MainWindow.xaml.cs
@@ -1,20 +1,33 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace task4_1
 {
     public partial class MainWindow : Window
     {
-        private task4_2.MainWindow secondWindow = new task4_2.MainWindow();
+        private IntPtr secondWindowHandle;
+        const int WM_USER = 0x0400; 
+
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
 
         public MainWindow()
         {
             InitializeComponent();
-            secondWindow.Show();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            secondWindow.ChangeStyles(TextBox1.Text, TextBox2.Text, TextBox3.Text);
+            secondWindowHandle = FindWindow(null, "MainWindowTask2");
+            if (secondWindowHandle == IntPtr.Zero)
+            {
+                MessageBox.Show("Вікно не знайдено.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            SendMessage(secondWindowHandle, WM_USER, IntPtr.Zero, IntPtr.Zero);
         }
     }
 }
--- 3/HomeWork3/task4_2/MainWindow.xaml.cs
+++ 3/HomeWork3/task4_2/MainWindow.xaml.cs
@@ -1,58 +1,58 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 using System.Windows.Media;
 
 namespace task4_2
 {
     public partial class MainWindow
     {
-        // Створюємо елементи керування, які успадковують властивості від базових класів
-        private Label _label = new Label();
-        private TextBox _textBox = new TextBox();
-        private Button _button = new Button();
+        private const int WM_USER = 0x0400;
 
         public MainWindow()
         {
-            // Додаємо елементи керування до вікна
-            StackPanel stackPanel = new StackPanel();
-            stackPanel.Children.Add(_label);
-            stackPanel.Children.Add(_textBox);
-            stackPanel.Children.Add(_button);
-            this.Content = stackPanel;
-
-            // Задаємо початкові значення для елементів керування
-            _label.Content = "Це вікно з успадкованими елементами керування";
-            _textBox.Text = "Введіть щось тут";
-            _button.Content = "Натисніть мене";
-            //InitializeComponent();
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
         }
 
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            source.AddHook(WndProc);
+        }
 
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_USER)
+            {
+                ChangeStyles("Red", "Times New Roman", "20");
+                handled = true;
+            }
 
-        // Метод, який змінює стилі елементів керування за параметрами
+            return IntPtr.Zero;
+        }
+
         public void ChangeStyles(string color, string font, string size)
         {
-            // Перевіряємо, чи є параметри валідними
             try
             {
                 Color textColor = (Color)ColorConverter.ConvertFromString(color);
-                _label.Foreground = new SolidColorBrush(textColor);
-                _textBox.Foreground = new SolidColorBrush(textColor);
-                _button.Foreground = new SolidColorBrush(textColor);
+                label.Foreground = new SolidColorBrush(textColor);
+                textBox.Foreground = new SolidColorBrush(textColor);
+                button.Foreground = new SolidColorBrush(textColor);
 
                 FontFamily fontFamily = new FontFamily(font);
-                _label.FontFamily = fontFamily;
-                _textBox.FontFamily = fontFamily;
-                _button.FontFamily = fontFamily;
+                label.FontFamily = fontFamily;
+                textBox.FontFamily = fontFamily;
+                button.FontFamily = fontFamily;
 
                 double fontSize = double.Parse(size);
-                _label.FontSize = fontSize;
-                _textBox.FontSize = fontSize;
-                _button.FontSize = fontSize;
+                label.FontSize = fontSize;
+                textBox.FontSize = fontSize;
+                button.FontSize = fontSize;
             }
             catch (Exception ex)
             {
-                // Якщо щось пішло не так, виводимо повідомлення про помилку
                 MessageBox.Show(ex.Message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
